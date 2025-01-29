715e249f0245d117bc8e803ee9568196ba920199
Alisa Raevskaya <bhumi.alisa@gmail.com>
2025-01-29 20:37:05 +0400

    Add role select

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,26 +1,34 @@
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, Link, useForm } from "@inertiajs/react";
+import Select from "@/Components/Select";
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
+        name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+        role_id: 2,
     });
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('register'), {
-            onFinish: () => reset('password', 'password_confirmation'),
+        post(route("register"), {
+            onFinish: () => reset("password", "password_confirmation"),
         });
     };
+
+    const rolesOptions = [
+        { label: "I want to borrow", value: 4, selected: true },
+        { label: "I want to lend", value: 3 },
+    ];
+    const defaultRole = 4;
 
     return (
         <GuestLayout>
@@ -37,7 +45,7 @@ export default function Register() {
                         className="mt-1 block w-full"
                         autoComplete="name"
                         isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
+                        onChange={(e) => setData("name", e.target.value)}
                         required
                     />
 
@@ -54,13 +62,12 @@ export default function Register() {
                         className="mt-1 block w-full"
                         autoComplete="last_name"
                         isFocused={true}
-                        onChange={(e) => setData('last_name', e.target.value)}
+                        onChange={(e) => setData("last_name", e.target.value)}
                         required
                     />
 
                     <InputError message={errors.name} className="mt-2" />
                 </div>
-
 
                 <div className="mt-4">
                     <InputLabel htmlFor="email" value="Email" />
@@ -72,7 +79,7 @@ export default function Register() {
                         value={data.email}
                         className="mt-1 block w-full"
                         autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
+                        onChange={(e) => setData("email", e.target.value)}
                         required
                     />
 
@@ -89,7 +96,7 @@ export default function Register() {
                         value={data.password}
                         className="mt-1 block w-full"
                         autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                         required
                     />
 
@@ -110,7 +117,7 @@ export default function Register() {
                         className="mt-1 block w-full"
                         autoComplete="new-password"
                         onChange={(e) =>
-                            setData('password_confirmation', e.target.value)
+                            setData("password_confirmation", e.target.value)
                         }
                         required
                     />
@@ -121,9 +128,27 @@ export default function Register() {
                     />
                 </div>
 
+                <div className="mt-4">
+                    <Select
+                        id="roleSelect"
+                        name="selectedRole"
+                        value={data.role_id}
+                        defaultValue={defaultRole}
+                        options={rolesOptions}
+                        className="mt-1 block w-full"
+                        onChange={(e) => setData("role_id", e.target.value)}
+                        required
+                    />
+
+                    {/* <InputError
+                        message={errors.password_confirmation}
+                        className="mt-2"
+                    /> */}
+                </div>
+
                 <div className="mt-4 flex items-center justify-end">
                     <Link
-                        href={route('login')}
+                        href={route("login")}
                         className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                         Already registered?
