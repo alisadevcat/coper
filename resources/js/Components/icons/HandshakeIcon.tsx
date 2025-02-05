3f396e1515f143a13bd12356c8e2114e9e434dae
Alisa Raevskaya <bhumi.alisa@gmail.com>
2025-02-06 00:17:54 +0400

    tweak styles

--- resources/js/Components/icons/HandshakeIcon.tsx
+++ resources/js/Components/icons/HandshakeIcon.tsx
@@ -3,6 +3,8 @@ import React from "react";
 export const HandshakeIcon = (props) => {
     return (
         <svg
+            width="48"
+            height="48"
             viewBox="0 0 48 48"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
--- resources/js/sections/home/AboutUs.tsx
+++ resources/js/sections/home/AboutUs.tsx
@@ -9,28 +9,54 @@ import Grid from "@mui/material/Grid2";
 
 const AboutUs = () => {
     return (
-        <Box id="about" sx={{ backgroundColor: "var(--ultra-dark" }}>
-            <Container maxWidth="sm"  sx={{
-                    color: "white",
-                    px: "80px",
-                }}
-            >
-            <Box
-                className="screen-container">
+        <Box
+            id="about"
+            sx={{
+                backgroundColor: "var(--ultra-dark",
+                mx: "auto",
+                width: "100%",
+                maxWidth: "1200px",
+            }}
+        >
+            <Box sx={{ color: "white", paddingBottom: "var(--s5)" }}>
+                <Box sx={{ marginBottom: "var(--s5)" }}>
+                    <Box>
+                        <Typography
+                            sx={{
+                                fontWeight: "500",
+                                opacity: "0.7",
+                                fontSize: "var(--h2)",
+                                marginBottom: "var(--s4)",
+                            }}
+                        >
+                            be cooperative!
+                        </Typography>
 
-                <Box className="row gutterbottom-big">
-                    <Box className="col-sm">
-                        <h2 className="h2 gutterbottom-md">be cooperative!</h2>
-                        <p className="p gutterbottom">
-                            The world today revolves around online interaction,
+                        <Typography
+                            sx={{
+
+                                lineHeight: 1.2,
+                                fontSize: "var(--p)",
+                                marginBottom: "var(--s3)",
+                            }}
+                        >
+                                 The world today revolves around online interaction,
                             with over 80% of the population utilising various
                             online services. However, there's a common need that
                             unites everyone: money. Regardless of religion,
                             race, or language, everyone requires financial means
                             for various purposes. Yet often, there's a crucial
                             shortage of funds when needed most.
-                        </p>
-                        <p className="p gutterbottom">
+                        </Typography>
+
+                        <Typography
+                            sx={{
+
+                                lineHeight: 1.2,
+                                fontSize: "var(--p)",
+                                marginBottom: "var(--s3)",
+                            }}
+                        >
                             For instance, someone might need money to pay rent
                             or buy groceries, while for others, it's essential
                             capital for a startup. In a world where you can book
@@ -42,17 +68,32 @@ const AboutUs = () => {
                             bypassing banking checks. On the platform, both
                             borrowers and lenders register for mutually
                             beneficial cooperation.
-                        </p>
-                        <p className="p">
+                            </Typography>
+                            <Typography
+                            sx={{
+
+                                lineHeight: 1.2,
+                                fontSize: "var(--p)",
+                            }}
+                        >
                             Coper is the future of financial services, where
                             your credit rating isn't as crucial as your ability
                             to lend a hand or receive help when needed.
-                        </p>
+                            </Typography>
                     </Box>
                 </Box>
-                <Box className="row gutterbottom-big">
+                <Box sx={{ marginBottom: "var(--s5)" }}>
                     <Box className="col-sm">
-                        <h2 className="h2 gutterbottom-md">about us</h2>
+                        <Typography
+                            sx={{
+                                fontWeight: "500",
+                                opacity: "0.7",
+                                fontSize: "var(--h2)",
+                                marginBottom: "var(--s4)",
+                            }}
+                        >
+                            about us{" "}
+                        </Typography>
                         <p className="p gutterbottom">
                             Coper was created by a team of promising developers
                             forced to leave Russia due to their disagreement
@@ -85,11 +126,12 @@ const AboutUs = () => {
                 </Box>
 
                 <Box
-                    className="row gutterbottom-big photos"
+                    className="photos"
                     sx={{
                         display: "flex",
                         gap: "var(--s5)",
                         justifyContent: "center",
+                        marginBottom: "var(--s5)",
                     }}
                 >
                     <Box
@@ -158,56 +200,60 @@ const AboutUs = () => {
                 </Box>
 
                 <Box className="row">
-                    <Box className="col-sm">
+                    <Typography
+                        sx={{
+                            fontWeight: "500",
+                            opacity: "0.7",
+                            fontSize: "var(--h2)",
+                        }}
+                    >
                         <h2 className="h2 gutterbottom-md">mission</h2>
-                        <p className="p gutterbottom">
-                            We aim to create a platform that unites people to
-                            provide assistance and support in various aspects of
-                            life, especially in challenging times. From
-                            supporting small businesses in Africa to helping
-                            students pay for education, Coper assists people in
-                            different corners of the world.
-                        </p>
-                        <p className="p gutterbottom">
-                            On one hand, our service isn't just about
-                            facilitating credit cooperation between two specific
-                            individuals but also about providing assistance and
-                            support when needed. We're creating a service that
-                            helps small businesses, students, mothers, fathers,
-                            cultural and artistic figures in their hour of need,
-                            serving all layers of society, regardless of race,
-                            nationality, citizenship, or religion.
-                        </p>
-                        <p className="p gutterbottom">
-                            On the other hand, our service involves investors,
-                            ensuring trust. The program not only offers the
-                            opportunity to earn above-market returns but also
-                            allows individuals to feel involved in doing
-                            something genuinely good by providing assistance and
-                            support in difficult times. After all, for someone,
-                            100 euros might be just a casino bet they'll lose
-                            with a 50% chance, while for someone else, such as a
-                            small business owner in an African country, it could
-                            be their weekly turnover, which could grow into
-                            something more significant thanks to timely
-                            investments. Project Coper is a cooperation platform
-                            born out of the need for help and support in
-                            difficult times. We bring people together, offering
-                            the opportunity to assist small businesses,
-                            students, families, and other segments of society.
-                            Our mission is to promote cooperation and create
-                            good deeds that benefit the entire world.
-                        </p>
-                        <p className="p">
-                            Transparency and safety are our priorities. We
-                            develop strict rules and checks to ensure safety and
-                            transparency for all project participants,
-                            minimizing risks and guaranteeing trust.
-                        </p>
-                    </Box>
+                    </Typography>
+                    <p className="p gutterbottom">
+                        We aim to create a platform that unites people to
+                        provide assistance and support in various aspects of
+                        life, especially in challenging times. From supporting
+                        small businesses in Africa to helping students pay for
+                        education, Coper assists people in different corners of
+                        the world.
+                    </p>
+                    <p className="p gutterbottom">
+                        On one hand, our service isn't just about facilitating
+                        credit cooperation between two specific individuals but
+                        also about providing assistance and support when needed.
+                        We're creating a service that helps small businesses,
+                        students, mothers, fathers, cultural and artistic
+                        figures in their hour of need, serving all layers of
+                        society, regardless of race, nationality, citizenship,
+                        or religion.
+                    </p>
+                    <p className="p gutterbottom">
+                        On the other hand, our service involves investors,
+                        ensuring trust. The program not only offers the
+                        opportunity to earn above-market returns but also allows
+                        individuals to feel involved in doing something
+                        genuinely good by providing assistance and support in
+                        difficult times. After all, for someone, 100 euros might
+                        be just a casino bet they'll lose with a 50% chance,
+                        while for someone else, such as a small business owner
+                        in an African country, it could be their weekly
+                        turnover, which could grow into something more
+                        significant thanks to timely investments. Project Coper
+                        is a cooperation platform born out of the need for help
+                        and support in difficult times. We bring people
+                        together, offering the opportunity to assist small
+                        businesses, students, families, and other segments of
+                        society. Our mission is to promote cooperation and
+                        create good deeds that benefit the entire world.
+                    </p>
+                    <p className="p">
+                        Transparency and safety are our priorities. We develop
+                        strict rules and checks to ensure safety and
+                        transparency for all project participants, minimizing
+                        risks and guaranteeing trust.
+                    </p>
                 </Box>
             </Box>
-            </Container>
         </Box>
     );
 };
